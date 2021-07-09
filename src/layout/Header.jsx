c733e462677aeb705c923d9f25988b288af0bb2b
AlexandrKirichenko <kiri4enkoalexandr@gmail.com>
2021-07-09 10:38:32 +0300

    pre-deploy settings

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,11 +6,14 @@ function Header(){
             <div className="nav-wrapper">
                 <img className="nav-logo" width={50} height={50} src="/img/mainlogo.svg" alt="logo"/>
                 <Link to="/">
-                    <a href="#" className="brand-logo">Fruit Shop</a>
+                    <span>Fruit Shop</span>
+
+
+
                 </Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <Link to="/Cart">
-                        <li><a href="!#">Cart</a></li>
+                        <span>Cart</span>
                     </Link>
                 </ul>
             </div>
