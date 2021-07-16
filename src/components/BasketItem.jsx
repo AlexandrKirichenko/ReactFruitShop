4d21b8b1e99679bb0291d88f751aa492ca163df6
AlexandrKirichenko <52669128+AlexandrKirichenko@users.noreply.github.com>
2021-07-16 18:11:05 +0300

    Update BasketItem.jsx

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,16 +1,47 @@
 import {useContext} from 'react';
 import {ShopContext} from '../context';
 
-function BasketItem({id, title,price,quantity}) {
+function BasketItem({id, title, price, quantity, sale}) {
     const {removeFromBasket, incQuantity, decQuantity} = useContext(ShopContext)
-    return <li  className="collection-item">
-    {title}<i className='material-icons basket-quantity' onClick={() => decQuantity(id)}>remove</i>
-      {quantity}кг.{' '}
-      <i className='material-icons basket-quantity' onClick={() => incQuantity(id)}>add</i> ={price * quantity}$
-    <span className="secondary-content" onClick={() => removeFromBasket(id)}>
-      <i className="material-icons basket-delete ">close</i>
-    </span>
-  </li>;
+
+    const discount = () => {
+        if (sale && quantity > 2) return Math.floor(quantity / 3) * 0.5 * price
+        else return 0
+    }
+
+    return (
+        <li className="collection-item">
+            {title}<i className='material-icons basket-quantity' onClick={() => decQuantity(id)}>remove</i>
+            {quantity}кг.{' '}
+            <i className='material-icons basket-quantity' onClick={() =>
+                incQuantity(id)}>add</i> = {price * quantity}$ {sale && discount()>0 && `(скидка ${discount()}$)`}
+            <span className="secondary-content" onClick={() => removeFromBasket(id)}>
+            <i className="material-icons basket-delete ">close</i>
+            </span>
+        </li>
+    )
 }
 
 export {BasketItem}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
