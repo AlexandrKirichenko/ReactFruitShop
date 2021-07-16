c7d06a7878e199da32dacdd0b22bb555175eff9e
AlexandrKirichenko <52669128+AlexandrKirichenko@users.noreply.github.com>
2021-07-16 09:34:49 +0300

    Update BasketItem.jsx

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -3,12 +3,10 @@ import {ShopContext} from '../context';
 
 function BasketItem({id, title,price,quantity}) {
     const {removeFromBasket, incQuantity, decQuantity} = useContext(ShopContext)
-    let discount = 0;
-    if (quantity > 2) discount = Math.floor(quantity / 3) * 0.5 * price
     return <li  className="collection-item">
     {title}<i className='material-icons basket-quantity' onClick={() => decQuantity(id)}>remove</i>
       {quantity}кг.{' '}
-      <i className='material-icons basket-quantity' onClick={() => incQuantity(id)}>add</i> ={price * quantity - discount}$
+      <i className='material-icons basket-quantity' onClick={() => incQuantity(id)}>add</i> ={price * quantity}$
     <span className="secondary-content" onClick={() => removeFromBasket(id)}>
       <i className="material-icons basket-delete ">close</i>
     </span>
