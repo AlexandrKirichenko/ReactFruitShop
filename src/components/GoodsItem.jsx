e8f5651d03680f1af17c04047d7ed07b56e31487
AlexandrKirichenko <52669128+AlexandrKirichenko@users.noreply.github.com>
2021-07-18 06:43:16 +0300

    Update GoodsItem.jsx

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,29 +1,37 @@
-import {useContext} from 'react';
-import {ShopContext} from '../context';
+import {useContext} from "react";
+import {ShopContext} from "../context"
 
-function BasketItem({id, title, price, quantity, sale}) {
-    const {removeFromBasket, incQuantity, decQuantity} = useContext(ShopContext)
-
-    const totalPrice = () => {
-        let discount = 0;
-        if (sale && quantity > 2) discount = Math.floor(quantity / 3) * 0.5 * price
-        return price * quantity - discount
-    }
+function GoodsItem({id, title, img, price, sale}) {
+    const {addToBasket} = useContext(ShopContext)
 
     return (
-        <li className="collection-item">
-            {title}<i className='material-icons basket-quantity' onClick={() => decQuantity(id)}>remove</i>
-            {quantity}кг.{' '}
-            <i className='material-icons basket-quantity' onClick={() =>
-                incQuantity(id)}>add</i> = {totalPrice()}$
-            <span className="secondary-content" onClick={() => removeFromBasket(id)}>
-            <i className="material-icons basket-delete ">close</i>
-            </span>
-        </li>
-    )
+        <div className="card">
+            {sale && (
+                <div className="ribbon-wrapper">
+                    <div className="ribbon">SALE</div>
+                </div>
+            )}
+            <div className="card-image">
+                <img src={img} alt={title} />
+            </div>
+            <div className="card-content ">
+                <span className="card-title">{title}</span>
+                {sale && (<span className="sale-text">Скидка 50% на каждый третий кг.!</span>)}
+            </div>
+            <div className="card-action">
+                <button
+                    className="btn"
+                    onClick={() => addToBasket({id, title, price, sale})}>
+                    Купить
+                </button>
+                <span className="right" style={{fontSize: '1.6rem'}}>{price}$</span>
+            </div>
+        </div>
+);
 }
 
-export {BasketItem}
+export {GoodsItem}
+
 
 
 
